86223d6397947fe070684401d95345b245521e7b
Harshavardhan_B <96969801+BayavarapuHarshavardhan@users.noreply.github.com>
2022-10-04 22:22:03 +0530

    Add files via upload

---
+++ magazine/forms.py
@@ -0,0 +1,8 @@
+from turtle import mode
+from django import forms
+from django.forms import ModelForm
+from .models import Magazine
+class MagazineForm(ModelForm):
+    class Meta:
+        model = Magazine
+        fields = "__all__"
---
+++ magazine/views.py
@@ -0,0 +1,27 @@
+from email.mime import image
+from turtle import title
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .forms import MagazineForm
+from .models import Magazine
+# Create your views here.
+def update(request):
+    if request.method =="POST":
+        info = MagazineForm(request.POST,request.FILES)
+        if info.is_valid():info.save()
+        mags = Magazine.objects.all()
+        return render(request,'index.html',{'mags':mags})
+    else:
+       return render(request,'update.html')
+def home(request):
+     mags = Magazine.objects.all()
+     return render(request, 'index.html',{'mags':mags})
+    
+def about(request):
+    return render(request,'about.html',{'name':'harshavardha bayavarapu'})
+
+
+
+
+    
